21937ca24bed837ed75be3719ad3c37f28ccf1a4
Teerapong Singthong <teerapong.s@linecorp.com>
2018-09-23 01:02:12 +0700

    feat(): add new test spec

---
+++ structmethodinterface/shapes.go
@@ -0,0 +1,5 @@
+package structmethodinterface
+
+func Perimeter(width float64, height float64) float64 {
+	return 2 * (width + height)
+}
---
+++ structmethodinterface/shapes_test.go
@@ -0,0 +1,12 @@
+package structmethodinterface
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f wamt %.2f", got, want)
+	}
+}
